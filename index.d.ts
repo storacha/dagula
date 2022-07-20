51f6b6baa105a11e97ca5804305647c32eadc165
Alan Shaw <alan.shaw@protocol.ai>
2022-07-20 22:10:14 +0100

    fix: types

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,10 @@
 import type { BlockDecoder } from 'multiformats/codecs/interface'
 import type { CID } from 'multiformats'
 import type { UnixFSEntry } from 'ipfs-unixfs-exporter'
+import type { Multiaddr } from '@multiformats/multiaddr'
 
 export interface BlockDecoders {
-  [code: number]: BlockDecoder
+  [code: number]: BlockDecoder<any, any>
 }
 
 export interface Block {
@@ -12,7 +13,7 @@ export interface Block {
 }
 
 export declare class Dagula {
-  constructor (peer?: string|Multiaddr, options?: { decoders?: BlockDecoders })
+  constructor (peer?: Multiaddr|string, options?: { decoders?: BlockDecoders })
   get (cid: CID|string, options?: { signal?: AbortSignal }): AsyncIterableIterator<Block>
   getBlock (cid: CID|string, options?: { signal?: AbortSignal }): Promise<Uint8Array>
   getUnixfs (path: CID|string, options?: { signal?: AbortSignal }): Promise<UnixFSEntry>
