3b758416cc2fb36b506dbca8bb3bc0023b3eac0b
Oli Evans <oli@protocol.ai>
2023-04-18 11:52:44 +0100

    chore: simplify tests
    License: MIT
    Signed-off-by: Oli Evans <oli@protocol.ai>

--- test/getPath.test.js
+++ test/getPath.test.js
@@ -1,9 +1,4 @@
 import test from 'ava'
-import { createLibp2p } from 'libp2p'
-import { webSockets } from '@libp2p/websockets'
-import { noise } from '@chainsafe/libp2p-noise'
-import { mplex } from '@libp2p/mplex'
-import { MemoryBlockstore } from 'blockstore-core/memory'
 import { fromString } from 'uint8arrays'
 import * as raw from 'multiformats/codecs/raw'
 import * as dagPB from '@ipld/dag-pb'
@@ -11,13 +6,12 @@ import { UnixFS } from 'ipfs-unixfs'
 import { sha256 } from 'multiformats/hashes/sha2'
 import { CID } from 'multiformats/cid'
 import * as Block from 'multiformats/block'
-import { Miniswap, BITSWAP_PROTOCOL } from 'miniswap'
 import { Dagula } from '../index.js'
 import { getLibp2p } from '../p2p.js'
+import { startBitswapPeer } from './_libp2p.js'
 
 test('should getPath', async t => {
   // should return all blocks in path and all blocks for resolved target of path
-  const serverBlockstore = new MemoryBlockstore()
   const filePart1 = await Block.decode({ codec: raw, bytes: fromString(`MORE TEST DATA ${Date.now()}`), hasher: sha256 })
   const filePart2 = await Block.decode({ codec: raw, bytes: fromString(`EVEN MORE TEST DATA ${Date.now()}`), hasher: sha256 })
   const fileNode = await Block.encode({
@@ -44,24 +38,10 @@ test('should getPath', async t => {
     }
   })
 
-  for (const { cid, bytes } of [filePart1, filePart2, fileNode, dirNode]) {
-    await serverBlockstore.put(cid, bytes)
-  }
-
-  const server = await createLibp2p({
-    addresses: { listen: ['/ip4/127.0.0.1/tcp/0/ws'] },
-    transports: [webSockets()],
-    streamMuxers: [mplex()],
-    connectionEncryption: [noise()]
-  })
-
-  const miniswap = new Miniswap(serverBlockstore)
-  server.handle(BITSWAP_PROTOCOL, miniswap.handler)
-
-  await server.start()
+  const peer = await startBitswapPeer([filePart1, filePart2, fileNode, dirNode])
 
   const libp2p = await getLibp2p()
-  const dagula = await Dagula.fromNetwork(libp2p, { peer: server.getMultiaddrs()[0] })
+  const dagula = await Dagula.fromNetwork(libp2p, { peer: peer.libp2p.getMultiaddrs()[0] })
   const entries = []
   for await (const entry of dagula.getPath(`${dirNode.cid}/foo`)) {
     entries.push(entry)
@@ -80,7 +60,6 @@ test('should getPath', async t => {
 
 test('should getPath on file with carScope=file', async t => {
   // return all blocks in path and all blocks for resolved target of path
-  const serverBlockstore = new MemoryBlockstore()
   const filePart1 = await Block.decode({ codec: raw, bytes: fromString(`MORE TEST DATA ${Date.now()}`), hasher: sha256 })
   const filePart2 = await Block.decode({ codec: raw, bytes: fromString(`EVEN MORE TEST DATA ${Date.now()}`), hasher: sha256 })
   const fileNode = await Block.encode({
@@ -107,24 +86,11 @@ test('should getPath on file with carScope=file', async t => {
     }
   })
 
-  for (const { cid, bytes } of [filePart1, filePart2, fileNode, dirNode]) {
-    await serverBlockstore.put(cid, bytes)
-  }
-
-  const server = await createLibp2p({
-    addresses: { listen: ['/ip4/127.0.0.1/tcp/0/ws'] },
-    transports: [webSockets()],
-    streamMuxers: [mplex()],
-    connectionEncryption: [noise()]
-  })
-
-  const miniswap = new Miniswap(serverBlockstore)
-  server.handle(BITSWAP_PROTOCOL, miniswap.handler)
-
-  await server.start()
+  const peer = await startBitswapPeer([filePart1, filePart2, fileNode, dirNode])
 
   const libp2p = await getLibp2p()
-  const dagula = await Dagula.fromNetwork(libp2p, { peer: server.getMultiaddrs()[0] })
+  const dagula = await Dagula.fromNetwork(libp2p, { peer: peer.libp2p.getMultiaddrs()[0] })
+
   const entries = []
   const carScope = 'file'
   for await (const entry of dagula.getPath(`${dirNode.cid}/foo`, { carScope })) {
@@ -144,7 +110,6 @@ test('should getPath on file with carScope=file', async t => {
 
 test('should getPath on file with carScope=block', async t => {
   // return all blocks in path and all blocks for resolved target of path
-  const serverBlockstore = new MemoryBlockstore()
   const filePart1 = await Block.decode({ codec: raw, bytes: fromString(`MORE TEST DATA ${Date.now()}`), hasher: sha256 })
   const filePart2 = await Block.decode({ codec: raw, bytes: fromString(`EVEN MORE TEST DATA ${Date.now()}`), hasher: sha256 })
   const fileNode = await Block.encode({
@@ -171,24 +136,10 @@ test('should getPath on file with carScope=block', async t => {
     }
   })
 
-  for (const { cid, bytes } of [filePart1, filePart2, fileNode, dirNode]) {
-    await serverBlockstore.put(cid, bytes)
-  }
-
-  const server = await createLibp2p({
-    addresses: { listen: ['/ip4/127.0.0.1/tcp/0/ws'] },
-    transports: [webSockets()],
-    streamMuxers: [mplex()],
-    connectionEncryption: [noise()]
-  })
-
-  const miniswap = new Miniswap(serverBlockstore)
-  server.handle(BITSWAP_PROTOCOL, miniswap.handler)
-
-  await server.start()
+  const peer = await startBitswapPeer([filePart1, filePart2, fileNode, dirNode])
 
   const libp2p = await getLibp2p()
-  const dagula = await Dagula.fromNetwork(libp2p, { peer: server.getMultiaddrs()[0] })
+  const dagula = await Dagula.fromNetwork(libp2p, { peer: peer.libp2p.getMultiaddrs()[0] })
   const entries = []
   const carScope = 'block'
   for await (const entry of dagula.getPath(`${dirNode.cid}/foo`, { carScope })) {
@@ -204,7 +155,6 @@ test('should getPath on file with carScope=block', async t => {
 
 test('should getPath on dir with carScope=file', async t => {
   // return all blocks in path. as it's a dir, it should stop there
-  const serverBlockstore = new MemoryBlockstore()
   const file = await Block.decode({ codec: raw, bytes: fromString(`MORE TEST DATA ${Date.now()}`), hasher: sha256 })
 
   const dirNode = await Block.encode({
@@ -219,24 +169,10 @@ test('should getPath on dir with carScope=file', async t => {
     }
   })
 
-  for (const { cid, bytes } of [file, dirNode]) {
-    await serverBlockstore.put(cid, bytes)
-  }
-
-  const server = await createLibp2p({
-    addresses: { listen: ['/ip4/127.0.0.1/tcp/0/ws'] },
-    transports: [webSockets()],
-    streamMuxers: [mplex()],
-    connectionEncryption: [noise()]
-  })
-
-  const miniswap = new Miniswap(serverBlockstore)
-  server.handle(BITSWAP_PROTOCOL, miniswap.handler)
-
-  await server.start()
+  const peer = await startBitswapPeer([file, dirNode])
 
   const libp2p = await getLibp2p()
-  const dagula = await Dagula.fromNetwork(libp2p, { peer: server.getMultiaddrs()[0] })
+  const dagula = await Dagula.fromNetwork(libp2p, { peer: peer.libp2p.getMultiaddrs()[0] })
   const entries = []
   for await (const entry of dagula.getPath(`${dirNode.cid}`, { carScope: 'file' })) {
     entries.push(entry)
--- test/index.test.js
+++ test/index.test.js
@@ -1,75 +1,41 @@
 import test from 'ava'
-import { createLibp2p } from 'libp2p'
-import { webSockets } from '@libp2p/websockets'
-import { noise } from '@chainsafe/libp2p-noise'
-import { mplex } from '@libp2p/mplex'
-import { MemoryBlockstore } from 'blockstore-core/memory'
 import { fromString, toString } from 'uint8arrays'
 import * as raw from 'multiformats/codecs/raw'
 import * as dagPB from '@ipld/dag-pb'
 import { UnixFS } from 'ipfs-unixfs'
 import { sha256 } from 'multiformats/hashes/sha2'
 import { CID } from 'multiformats/cid'
-import { Miniswap, BITSWAP_PROTOCOL } from 'miniswap'
 import { TimeoutController } from 'timeout-abort-controller'
 import { Dagula } from '../index.js'
 import { getLibp2p } from '../p2p.js'
+import { startBitswapPeer } from './_libp2p.js'
 
 test('should fetch a single CID', async t => {
-  // create blockstore and add data
-  const serverBlockstore = new MemoryBlockstore()
-  const data = fromString(`TEST DATA ${Date.now()}`)
-  const hash = await sha256.digest(data)
+  const bytes = fromString(`TEST DATA ${Date.now()}`)
+  const hash = await sha256.digest(bytes)
   const cid = CID.create(1, raw.code, hash)
-  await serverBlockstore.put(cid, data)
-
-  const server = await createLibp2p({
-    addresses: { listen: ['/ip4/127.0.0.1/tcp/0/ws'] },
-    transports: [webSockets()],
-    streamMuxers: [mplex()],
-    connectionEncryption: [noise()]
-  })
-
-  const miniswap = new Miniswap(serverBlockstore)
-  server.handle(BITSWAP_PROTOCOL, miniswap.handler)
-
-  await server.start()
+  const peer = await startBitswapPeer([{ cid, bytes }])
 
   const libp2p = await getLibp2p()
-  const dagula = await Dagula.fromNetwork(libp2p, { peer: server.getMultiaddrs()[0] })
+  const dagula = await Dagula.fromNetwork(libp2p, { peer: peer.libp2p.getMultiaddrs()[0] })
   for await (const block of dagula.get(cid)) {
     t.is(block.cid.toString(), cid.toString())
-    t.is(toString(block.bytes), toString(data))
+    t.is(toString(block.bytes), toString(bytes))
   }
 })
 
 test('should walk a unixfs path', async t => {
-  // create blockstore and add data
-  const serverBlockstore = new MemoryBlockstore()
-  const data = fromString(`TEST DATA ${Date.now()}`)
-  const hash = await sha256.digest(data)
+  const bytes = fromString(`TEST DATA ${Date.now()}`)
+  const hash = await sha256.digest(bytes)
   const cid = CID.create(1, raw.code, hash)
-  await serverBlockstore.put(cid, data)
   const linkName = 'foo'
   const dirData = new UnixFS({ type: 'directory' }).marshal()
   const dirBytes = dagPB.encode(dagPB.prepare({ Data: dirData, Links: [{ Name: linkName, Hash: cid }] }))
   const dirCid = CID.create(1, dagPB.code, await sha256.digest(dirBytes))
-  await serverBlockstore.put(dirCid, dirBytes)
-
-  const server = await createLibp2p({
-    addresses: { listen: ['/ip4/127.0.0.1/tcp/0/ws'] },
-    transports: [webSockets()],
-    streamMuxers: [mplex()],
-    connectionEncryption: [noise()]
-  })
-
-  const miniswap = new Miniswap(serverBlockstore)
-  server.handle(BITSWAP_PROTOCOL, miniswap.handler)
-
-  await server.start()
+  const peer = await startBitswapPeer([{ cid: dirCid, bytes: dirBytes }, { cid, bytes }])
 
   const libp2p = await getLibp2p()
-  const dagula = await Dagula.fromNetwork(libp2p, { peer: server.getMultiaddrs()[0] })
+  const dagula = await Dagula.fromNetwork(libp2p, { peer: peer.libp2p.getMultiaddrs()[0] })
   const entries = []
   for await (const entry of dagula.walkUnixfsPath(`${dirCid}/${linkName}`)) {
     entries.push(entry)
@@ -78,24 +44,13 @@ test('should walk a unixfs path', async t => {
   t.deepEqual(entries.at(0).cid, dirCid)
   t.deepEqual(entries.at(0).bytes, dirBytes)
   t.deepEqual(entries.at(1).cid, cid)
-  t.deepEqual(entries.at(1).bytes, data)
+  t.deepEqual(entries.at(1).bytes, bytes)
 })
 
 test('should abort a fetch', async t => {
-  const server = await createLibp2p({
-    addresses: { listen: ['/ip4/127.0.0.1/tcp/0/ws'] },
-    transports: [webSockets()],
-    streamMuxers: [mplex()],
-    connectionEncryption: [noise()]
-  })
-
-  const miniswap = new Miniswap(new MemoryBlockstore())
-  server.handle(BITSWAP_PROTOCOL, miniswap.handler)
-
-  await server.start()
-
+  const peer = await startBitswapPeer()
   const libp2p = await getLibp2p()
-  const dagula = await Dagula.fromNetwork(libp2p, { peer: server.getMultiaddrs()[0] })
+  const dagula = await Dagula.fromNetwork(libp2p, { peer: peer.libp2p.getMultiaddrs()[0] })
   // not in the blockstore so will hang indefinitely
   const cid = 'bafkreig7tekltu2k2bci74rpbyrruft4e7nrepzo4z36ie4n2bado5ru74'
   const controller = new TimeoutController(1_000)
